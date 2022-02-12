d6c99ba075825e52bd4442146d9918c228695354
tasuqilt <99556271+tasuqilt@users.noreply.github.com>
2022-02-12 14:20:51 -0500

    Add files via upload

---
+++ detect_lang.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Testing detecting "kab" language
+
+
+#pip install langdetect
+from langdetect import detect
+
+print(detect("bɣiɣ ad ṛuḥeɣ ar uɣerbaz s tkeṛṛust mačči ɣef uḍaṛ."))
+
+print('completed')
